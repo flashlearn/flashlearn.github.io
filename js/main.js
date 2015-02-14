be5bc58ed9825c964cae6b6fdeeb3bde63d3c3c1
JemarJones <jemarkjones@gmail.com>
2015-02-14 18:35:28 -0500

    small updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var catagories = ["Duck 1","Duck 2","Duck 3","Duck 4"];
 var catImages = ["/images/duck1.jpg","/images/duck2.jpg","/images/duck3.jpg","/images/duck4.jpg"];
 $(document).ready(function(){
-
+	for (var i = 0; i <catagories.length; ++i){
+		var newCat = $('<div class="cat"></div>');
+		newCat.addClass
+	}
 });
