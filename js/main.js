0d4c54664475317047057a24edf4b34f3627d143
JemarJones <jemarkjones@gmail.com>
2015-02-14 17:48:01 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var catagories = ["Duck 1","Duck 2","Duck 3","Duck 4"];
+var catImages = ["/images/duck1.jpg","/images/duck2.jpg","/images/duck3.jpg","/images/duck4.jpg"];
+$(document).ready(function(){
+
+});
