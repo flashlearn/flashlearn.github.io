8737de7db1ce54ba38c43c2974a97e85620961fb
JemarJones <jemarkjones@gmail.com>
2015-02-14 20:02:11 -0500

    Added automatic category loading

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 var catagories = ["Duck 1","Duck 2","Duck 3","Duck 4"];
-var catImages = ["/images/duck1.jpg","/images/duck2.jpg","/images/duck3.jpg","/images/duck4.jpg"];
+var catImages = ["./images/duck1.jpg","./images/duck2.jpg","./images/duck3.jpg","./images/duck4.jpg"];
 $(document).ready(function(){
 	for (var i = 0; i <catagories.length; ++i){
-		var newCat = $('<div class="cat"></div>');
-		newCat.addClass
+		var newCat = $('<div></div>');
+		newCat.addClass('cat');
+		var pic = $('<img>');
+		pic.addClass('catPic');
+		pic.attr("src",catImages[i]);
+		newCat.append(pic);
+		var name = $('<p></p>');
+		name.addClass('catname');
+		name.text(catagories[i]);
+		newCat.append(name);
+		$('#catContainer').append(newCat);
 	}
 });
