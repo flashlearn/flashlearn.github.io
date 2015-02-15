9c0380aca5210856f8d5a3c67972a1b0e98cb38e
JemarJones <jemarkjones@gmail.com>
2015-02-15 00:41:58 -0500

    Added subcategory interface

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var catagories = ["Duck 1","Duck 2","Duck 3","Duck 4","Duck 5"];
 var catImages = ["./images/duck1.jpg","./images/duck2.jpg","./images/duck3.jpg","./images/duck4.jpg","./images/duck5.jpg"];
+var subcatagories = [["flying","being yellow","flying","being yellow"],["flying2","being yellow2"],["flying3","being yellow3"],["flying4","being yellow4"],["flying5","being yellow5"]];
+var catClicked = false;
 $(document).ready(function(){
 	for (var i = 0; i <catagories.length; ++i){
 		var newCat = $('<div></div>');
@@ -13,5 +15,44 @@ $(document).ready(function(){
 		name.text(catagories[i]);
 		newCat.append(name);
 		$('#catContainer').append(newCat);
+		var newSubCats = $('<div></div>');
+		newSubCats.addClass('subcat');
+		var list = $('<ul></ul>');
+		list.attr("id","left");
+		var list2 = $('<ul></ul>');
+		list2.attr("id","right");
+		for (var k = 0; k <subcatagories[i].length;++k){
+			var listItem = $('<li></li>');
+			var link = $('<a></a>');
+			link.attr("href", "#");
+			link.text(subcatagories[i][k]);
+			listItem.append(link);
+			if (k % 2 === 0){
+				list.append(listItem);
+			} else{
+				list2.append(listItem);
+			}
+		}
+		newSubCats.css("display","none");
+		newSubCats.append(list);
+		newSubCats.append(list2);
+		$('#catContainer').append(newSubCats);
 	}
+	$('.cat').on('click',function(){
+		var mySubCat = $(this).next('.subcat');
+		var thisCat = $(this);
+		if (!catClicked){
+			$('.cat').fadeOut(600,function(){
+				thisCat.fadeIn(600);
+				mySubCat.slideDown(600);
+			});
+		}else{
+			mySubCat.slideUp(600);
+			thisCat.fadeOut(600, function(){
+				$('.cat').fadeIn(600);
+			});
+		}
+		catClicked = !catClicked;
+	});
+
 });
