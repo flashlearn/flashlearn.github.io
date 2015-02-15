7c9e4363e9efdcc03a67485697ff5c3f210a02d8
bickybilly <bebefro@gmail.com>
2015-02-15 02:40:03 -0600

    Next/Previous Card

--- js/cards.js
+++ js/cards.js
@@ -3,13 +3,24 @@ var questions = ["Who is this?","What is this duck's name?","Where did the duck 
 var images = ["./images/duck1.jpg","./images/duck2.jpg","",""];
 var answers = ["Mr. Duck!","Henry!","He went swimming in Lake McMaster.","Kraft Dinner."];
 var words = ["", "", "...", "...specifically, Mr.Duck and Henry. If Henry brought peanut butter and jelly sandwiches, what did Mr. Duck bring?"];
-var count = 0;
+var count = -1;
 $(document).ready(function(){
 
-	$("#navName").click(function(){
+	var changeCard = function (){
 		$(".answer").hide()
 		// current question
-		$(".question").text(questions[count % questions.length]);
+		if ($(this).get(0) === $("#next").get(0)){
+			$(".question").text(questions[++count % questions.length]);
+		}
+		else {
+			$(".question").text(questions[--count % questions.length]);	
+		}
+		if (count>0){
+			$("#prev").show();
+		}
+		else {
+			$("#prev").hide();
+		}
 		$(".question").show();
 		// current qImg
 		if (images[count % questions.length] != ""){
@@ -24,14 +35,19 @@ $(document).ready(function(){
 			$(".qBody").show();
 		}
 		// current answer
-		$(".answer").text(answers[count++ % questions.length]);
-	});
+			$(".answer").text(answers[count % questions.length]);
+		
+	}
+
+	$("#next").click(changeCard);
+	$("#prev").click(changeCard);
 
 	$("#card").click(function(){
 		$("#card").flip({
 			direction:'bt',
 			color:'white',
 			onBefore: function(){
+				// alert("Before");
 				$(".question").hide();
 			 	$(".qImg").hide();
 				$(".qBody").hide();
