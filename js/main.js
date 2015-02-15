dccf70683b8e0e4fbb8e60f2fd91480fea89e0b5
JemarJones <jemarkjones@gmail.com>
2015-02-14 20:15:10 -0500

    Added better styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var catagories = ["Duck 1","Duck 2","Duck 3","Duck 4"];
-var catImages = ["./images/duck1.jpg","./images/duck2.jpg","./images/duck3.jpg","./images/duck4.jpg"];
+var catagories = ["Duck 1","Duck 2","Duck 3","Duck 4","Duck 5"];
+var catImages = ["./images/duck1.jpg","./images/duck2.jpg","./images/duck3.jpg","./images/duck4.jpg","./images/duck5.jpg"];
 $(document).ready(function(){
 	for (var i = 0; i <catagories.length; ++i){
 		var newCat = $('<div></div>');
