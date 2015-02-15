66181dd193a27cae591f216dc34f9166c15956c2
bickybilly <bebefro@gmail.com>
2015-02-15 11:46:03 -0600

    Math icon

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var catagories = ["Chemistry","History","Geography","Duck 4","Duck 5"];
-var catImages = ["./images/chem.gif","./images/history.gif","./images/geo.gif","./images/duck4.jpg","./images/duck5.jpg"];
+var catImages = ["./images/chem.gif","./images/history.gif","./images/geo.gif","./images/math.gif"];
 var subcatagories = [["flying","being yellow","flying","being yellow"],["flying2","being yellow2"],["flying3","being yellow3"],["flying4","being yellow4"],["flying5","being yellow5"]];
 var catClicked = false;
 $(document).ready(function(){
