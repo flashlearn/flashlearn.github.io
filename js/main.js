5bbd91a571624cb264062fafabe414877348e36f
JemarJones <jemarkjones@gmail.com>
2015-02-15 11:51:32 -0500

    Fixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var catagories = ["Chemistry","Math","History","Geography"];
-var catImages = ["./images/chem.gif","./images/history.gif","./images/geo.gif","./images/duck4.jpg","./images/duck5.jpg"];
+var catImages = ["./images/chem.gif","./images/math.gif","./images/history.gif","./images/geo.gif"];
 var subcatagories = [["Organic Chemistry","","Bio Chemistry","Electro Chemistry"],["Calculus","Trig"],["World War 2","American History"],["Techtonics","Mountain Ranges"]];
 var catClicked = false;
 $(document).ready(function(){
