423213b033aa74a0551d21b23674724164bee54a
bickybilly <bebefro@gmail.com>
2015-02-15 11:27:14 -0600

    plus logos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var catagories = ["Duck 1","Duck 2","Duck 3","Duck 4","Duck 5"];
-var catImages = ["./images/duck1.jpg","./images/duck2.jpg","./images/duck3.jpg","./images/duck4.jpg","./images/duck5.jpg"];
+var catagories = ["Chemistry","History","Duck 3","Duck 4","Duck 5"];
+var catImages = ["./images/chem.gif","./images/history.gif","./images/duck3.jpg","./images/duck4.jpg","./images/duck5.jpg"];
 var subcatagories = [["flying","being yellow","flying","being yellow"],["flying2","being yellow2"],["flying3","being yellow3"],["flying4","being yellow4"],["flying5","being yellow5"]];
 var catClicked = false;
 $(document).ready(function(){
